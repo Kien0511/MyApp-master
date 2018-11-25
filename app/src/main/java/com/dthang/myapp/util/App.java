0d9eda02536c6ed71735afa7c985ceadc6fbbb44
Kien0511 <45326730+Kien0511@users.noreply.github.com>
2018-11-25 16:10:05 +0700

    Add files via upload

--- app/src/main/java/com/dthang/myapp/util/App.java
+++ app/src/main/java/com/dthang/myapp/util/App.java
@@ -14,5 +14,6 @@ public class App extends Application {
     public static final String _URL = "http://vnchild.com/myapp/query.php";
     public static final String NAME_SHAREDPREFERENCES = "login";
     public static final String LOGTAG = "dulieu";
-    public static final String SERVER = "http://192.168.1.111:81/myapp";
+    public static final String SERVER1 = "http://192.168.1.111:81/myapp";
+    public static final String SERVER = "http://vnchild.com/myapp";
 }
